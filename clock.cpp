5ea5b4e4a7f5d5eebebbd4055bcab5017e718a0a
fairy <fujikawa_mia@outlook.com>
2025-12-19 09:01:34 +0800

    update

--- clock.cpp
+++ clock.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 int clock(vector<int> &pages, int frames) {
+  cout << "Clock算法执行过程:" << endl;
   int n = pages.size();
   vector<int> memory(frames, -1);
   vector<bool> reference(frames, false);
@@ -18,20 +19,39 @@ int clock(vector<int> &pages, int frames) {
         break;
       }
     }
+    
+    cout << "访问页面 " << page << ": ";
     if (!found) {
       faults++;
+      cout << "需要置换, ";
       while (reference[pointer]) {
+        cout << "跳过页面 " << memory[pointer] << "(引用位=1), ";
         reference[pointer] = false;
         pointer = (pointer + 1) % frames;
       }
+      cout << "置换页面 " << memory[pointer] << " 为页面 " << page;
       memory[pointer] = page;
       reference[pointer] = true;
       pointer = (pointer + 1) % frames;
+      cout << " [缺页]";
+    } else {
+      cout << "页面已在内存中";
     }
+    
+    cout << " 内存状态: [";
+    for (int j = 0; j < frames; j++) {
+      if (j > 0) cout << " ";
+      if (memory[j] == -1) 
+        cout << "-";
+      else 
+        cout << memory[j] << "(" << reference[j] << ")";
+    }
+    cout << "] 指针位置: " << ((pointer-1+frames)%frames) << endl;
   }
   return faults;
 }
 int enhanced_clock(vector<int> &pages, int frames) {
+  cout << "Enhanced Clock算法执行过程:" << endl;
   int n = pages.size();
   vector<int> memory(frames, -1);
   vector<bool> reference(frames, false);
@@ -52,16 +72,21 @@ int enhanced_clock(vector<int> &pages, int frames) {
         break;
       }
     }
+    
+    cout << "访问页面 " << page << ": ";
     if (!found) {
       faults++;
+      cout << "需要置换, ";
       int scan_count = 0;
       bool page_replaced = false;
       while (!page_replaced) {
         scan_count++;
         switch (scan_count) {
         case 1:
+          cout << "第一轮扫描寻找(0,0), ";
           for (int j = 0; j < frames; j++) {
             if (!reference[j] && !modified[j]) {
+              cout << "找到页面 " << memory[j] << ", ";
               memory[j] = page;
               reference[j] = true;
               modified[j] = false;
@@ -71,8 +96,10 @@ int enhanced_clock(vector<int> &pages, int frames) {
           }
           break;
         case 2:
+          cout << "第二轮扫描寻找(0,1), ";
           for (int j = 0; j < frames; j++) {
             if (!reference[j] && modified[j]) {
+              cout << "找到页面 " << memory[j] << ", ";
               memory[j] = page;
               reference[j] = true;
               modified[j] = false;
@@ -83,8 +110,10 @@ int enhanced_clock(vector<int> &pages, int frames) {
           }
           break;
         case 3:
+          cout << "第三轮扫描寻找(0,0), ";
           for (int j = 0; j < frames; j++) {
             if (!modified[j] && !reference[j]) {
+              cout << "找到页面 " << memory[j] << ", ";
               memory[j] = page;
               reference[j] = true;
               modified[j] = false;
@@ -94,8 +123,10 @@ int enhanced_clock(vector<int> &pages, int frames) {
           }
           break;
         case 4:
+          cout << "第四轮扫描寻找(0,1), ";
           for (int j = 0; j < frames; j++) {
             if (modified[j] && !reference[j]) {
+              cout << "找到页面 " << memory[j] << ", ";
               memory[j] = page;
               reference[j] = true;
               modified[j] = false;
@@ -106,13 +137,26 @@ int enhanced_clock(vector<int> &pages, int frames) {
           break;
         }
       }
+      cout << "置换为页面 " << page;
+      cout << " [缺页]";
+    } else {
+      cout << "页面已在内存中";
     }
+    
+    cout << " 内存状态: [";
+    for (int j = 0; j < frames; j++) {
+      if (j > 0) cout << " ";
+      if (memory[j] == -1) 
+        cout << "-";
+      else 
+        cout << memory[j] << "(" << reference[j] << "," << modified[j] << ")";
+    }
+    cout << "]" << endl;
   }
   return faults;
 }
 void testAlgorithm(string name, int (*algo)(vector<int> &, int),
                    vector<int> &pages, int frames) {
-  int faults = algo(pages, frames);
   cout << name << "算法:" << endl;
   cout << "页面序列: ";
   for (int page : pages) {
@@ -120,9 +164,12 @@ void testAlgorithm(string name, int (*algo)(vector<int> &, int),
   }
   cout << endl;
   cout << "物理块数: " << frames << endl;
+  
+  int faults = algo(pages, frames);
+  
   cout << "缺页次数: " << faults << endl;
   cout << "缺页率: " << (double)faults / pages.size() * 100 << "%" << endl;
-  cout << "------------------------" << endl;
+  cout << "========================" << endl;
 }
 int main() {
   vector<int> pages = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
--- pages.cpp
+++ pages.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 using namespace std;
 int opt(vector<int> &pages, int frames) {
+  cout << "OPT算法执行过程:" << endl;
   int n = pages.size();
   vector<int> memory(frames, -1);
   int faults = 0;
@@ -19,11 +20,13 @@ int opt(vector<int> &pages, int frames) {
         break;
       }
     }
+    cout << "访问页面 " << page << ": ";
     if (!found) {
       faults++;
       if (occupiedCount < frames) {
         memory[occupiedCount] = page;
         occupiedCount++;
+        cout << "装入页面 " << page << " 到位置 " << occupiedCount - 1;
       } else {
         int farthest = i;
         int replaceIndex = 0;
@@ -40,55 +43,115 @@ int opt(vector<int> &pages, int frames) {
             replaceIndex = j;
           }
         }
+        cout << "替换页面 " << memory[replaceIndex] << " 为页面 " << page
+             << " (位置 " << replaceIndex << ")";
         memory[replaceIndex] = page;
       }
+      cout << " [缺页]";
+    } else {
+      cout << "页面已在内存中";
     }
+    cout << " 内存状态: [";
+    for (int j = 0; j < frames; j++) {
+      if (j > 0)
+        cout << " ";
+      if (memory[j] == -1)
+        cout << "-";
+      else
+        cout << memory[j];
+    }
+    cout << "]" << endl;
   }
   return faults;
 }
 int fifo(vector<int> &pages, int frames) {
+  cout << "FIFO算法执行过程:" << endl;
   int n = pages.size();
   list<int> frame;
   unordered_map<int, bool> inFrame;
   int faults = 0;
   for (int i = 0; i < n; i++) {
     int page = pages[i];
+    cout << "访问页面 " << page << ": ";
     if (!inFrame[page]) {
       faults++;
       if (frame.size() == frames) {
         int removed = frame.front();
         frame.pop_front();
         inFrame[removed] = false;
+        cout << "移除页面 " << removed << ", ";
       }
       frame.push_back(page);
       inFrame[page] = true;
+      cout << "装入页面 " << page;
+      cout << " [缺页]";
+    } else {
+      cout << "页面已在内存中";
     }
+    cout << " 内存状态: [";
+    int count = 0;
+    for (int f : frame) {
+      if (count > 0)
+        cout << " ";
+      cout << f;
+      count++;
+    }
+    // 填充空位
+    while (count < frames) {
+      if (count > 0)
+        cout << " ";
+      cout << "-";
+      count++;
+    }
+    cout << "]" << endl;
   }
   return faults;
 }
 int lru(vector<int> &pages, int frames) {
+  cout << "LRU算法执行过程:" << endl;
   int n = pages.size();
   list<int> frame;
   unordered_map<int, list<int>::iterator> pagePos;
   int faults = 0;
   for (int i = 0; i < n; i++) {
     int page = pages[i];
+    cout << "访问页面 " << page << ": ";
     if (pagePos.find(page) == pagePos.end()) {
       faults++;
       if (frame.size() == frames) {
         int removed = frame.front();
         frame.pop_front();
         pagePos.erase(removed);
+        cout << "移除页面 " << removed << ", ";
       }
+      cout << "装入页面 " << page;
+      cout << " [缺页]";
     } else {
       frame.erase(pagePos[page]);
+      cout << "页面 " << page << " 移至最近使用位置";
     }
     frame.push_back(page);
     pagePos[page] = --frame.end();
+    cout << " 内存状态: [";
+    int count = 0;
+    for (int f : frame) {
+      if (count > 0)
+        cout << " ";
+      cout << f;
+      count++;
+    }
+    while (count < frames) {
+      if (count > 0)
+        cout << " ";
+      cout << "-";
+      count++;
+    }
+    cout << "]" << endl;
   }
   return faults;
 }
 int lfu(vector<int> &pages, int frames) {
+  cout << "LFU算法执行过程:" << endl;
   int n = pages.size();
   vector<int> memory(frames, -1);
   vector<int> frequency(frames, 0);
@@ -100,45 +163,61 @@ int lfu(vector<int> &pages, int frames) {
   for (int i = 0; i < n; i++) {
     int page = pages[i];
     counter++;
+    cout << "访问页面 " << page << ": ";
     if (pageMap.find(page) != pageMap.end()) {
       int index = pageMap[page];
       frequency[index]++;
       timeStamp[index] = counter;
-      continue;
-    }
-    faults++;
-    if (occupiedCount < frames) {
-      memory[occupiedCount] = page;
-      frequency[occupiedCount] = 1;
-      timeStamp[occupiedCount] = counter;
-      pageMap[page] = occupiedCount;
-      occupiedCount++;
+      cout << "页面已在内存中, 频率更新为 " << frequency[index];
     } else {
-      int minFreq = INT_MAX;
-      int oldest = counter + 1;
-      int replaceIndex = 0;
-      for (int j = 0; j < frames; j++) {
-        if (frequency[j] < minFreq) {
-          minFreq = frequency[j];
-          replaceIndex = j;
-          oldest = timeStamp[j];
-        } else if (frequency[j] == minFreq && timeStamp[j] < oldest) {
-          replaceIndex = j;
-          oldest = timeStamp[j];
+      faults++;
+      if (occupiedCount < frames) {
+        memory[occupiedCount] = page;
+        frequency[occupiedCount] = 1;
+        timeStamp[occupiedCount] = counter;
+        pageMap[page] = occupiedCount;
+        occupiedCount++;
+        cout << "装入页面 " << page << " 到位置 " << occupiedCount - 1;
+      } else {
+        int minFreq = INT_MAX;
+        int oldest = counter + 1;
+        int replaceIndex = 0;
+        for (int j = 0; j < frames; j++) {
+          if (frequency[j] < minFreq) {
+            minFreq = frequency[j];
+            replaceIndex = j;
+            oldest = timeStamp[j];
+          } else if (frequency[j] == minFreq && timeStamp[j] < oldest) {
+            replaceIndex = j;
+            oldest = timeStamp[j];
+          }
         }
+        cout << "替换页面 " << memory[replaceIndex]
+             << " (频率=" << frequency[replaceIndex] << ") 为页面 " << page
+             << " (位置 " << replaceIndex << ")";
+        pageMap.erase(memory[replaceIndex]);
+        memory[replaceIndex] = page;
+        frequency[replaceIndex] = 1;
+        timeStamp[replaceIndex] = counter;
+        pageMap[page] = replaceIndex;
       }
-      pageMap.erase(memory[replaceIndex]);
-      memory[replaceIndex] = page;
-      frequency[replaceIndex] = 1;
-      timeStamp[replaceIndex] = counter;
-      pageMap[page] = replaceIndex;
+      cout << " [缺页]";
     }
+    cout << " 内存状态: [";
+    for (int j = 0; j < frames; j++) {
+      if (j > 0)
+        cout << " ";
+      if (memory[j] == -1)
+        cout << "-";
+      else
+        cout << memory[j] << "(" << frequency[j] << ")";
+    }
+    cout << "]" << endl;
   }
   return faults;
 }
 void testAlgorithm(string name, int (*algo)(vector<int> &, int),
                    vector<int> &pages, int frames) {
-  int faults = algo(pages, frames);
   cout << name << "算法:" << endl;
   cout << "页面序列: ";
   for (int page : pages) {
@@ -146,9 +225,10 @@ void testAlgorithm(string name, int (*algo)(vector<int> &, int),
   }
   cout << endl;
   cout << "物理块数: " << frames << endl;
+  int faults = algo(pages, frames);
   cout << "缺页次数: " << faults << endl;
   cout << "缺页率: " << (double)faults / pages.size() * 100 << "%" << endl;
-  cout << "------------------------" << endl;
+  cout << "========================" << endl;
 }
 int main() {
   vector<int> pages = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
